6a58683bc510bcfb04c97737a517596260cddc83
Luiza Pozzobon <luiza.pozzobon@gmail.com>
2022-06-07 15:42:19 -0300

    chore: add requirements

--- run_horovod.sh
+++ run_horovod.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0,1 horovodrun -np 2 -H localhost:2 python dreamerv2/train.py --logdir /logdir/atari_riverraid/dreamerv2/teste --configs atari --task atari_riverraid
+CUDA_VISIBLE_DEVICES=0,1 horovodrun -np 2 -H localhost:2 python3 dreamerv2/train.py --logdir ~/logdir/atari_riverraid/horovod/1 --configs atari --task atari_riverraid
